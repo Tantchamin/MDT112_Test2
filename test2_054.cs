e51d45dcf62c1ad39165d9fbd99314493493396b
Tantchamin <tantchamin@gmail.com>
2021-03-28 12:24:36 +0700

    MDT112-Test2

---
+++ test2_054.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Test2_054
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double x = a;
+            double y = b;
+
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
